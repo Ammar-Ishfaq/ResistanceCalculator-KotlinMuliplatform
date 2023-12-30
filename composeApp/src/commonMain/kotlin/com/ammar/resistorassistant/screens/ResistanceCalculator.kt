a38dbd41032308ca7531408b4d5046621139d8fa
ammar <ammarishfaq25@gmail.com>
2023-12-30 14:51:36 +0500

    - bottom bar implemented

--- composeApp/src/commonMain/kotlin/com/ammar/resistorassistant/screens/ResistanceCalculator.kt
+++ composeApp/src/commonMain/kotlin/com/ammar/resistorassistant/screens/ResistanceCalculator.kt
@@ -1,125 +1,162 @@
 package com.ammar.resistorassistant.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.BottomAppBar
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key.Companion.R
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.ammar.resistorassistant.MR
+import com.ammar.resistorassistant.extension.toCR
+import com.ammar.resistorassistant.extension.toComposeColor
 import dev.icerock.moko.graphics.parseColor
-import dev.icerock.moko.resources.ImageResource
-import dev.icerock.moko.resources.compose.colorResource
-import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.graphics.Color as MokoColor
 
 data object ResistanceCalculator : Screen {
     @Composable
     override fun Content() {
-        Column(modifier = Modifier.fillMaxSize()) {
-            var selectedBandCount by remember { mutableStateOf(4) }
-            ResistanceCalculator(selectedBandCount)
-            Spacer(modifier = Modifier.height(16.dp))
-            BottomBar {
-                selectedBandCount = it
+        var selectedScreen by remember { mutableStateOf<ScreenType>(ScreenType.HOME) }
+
+        val isHome= selectedScreen == ScreenType.HOME
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column {
+                // Title header
+                Text(
+                    text = if(isHome)"Resistance Calculate" else "Menu", // Replace with your desired title
+                    style = MaterialTheme.typography.h4,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+                // Black box with padding
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f) // Takes up remaining space
+                        .padding(16.dp)
+                ) {
+                    // Content for the box (e.g., screen-specific content)
+                    when (selectedScreen) {
+                        ScreenType.HOME -> {
+                            Text("HomeScreen Content")
+                        }
+                        ScreenType.MORE -> {
+                            Text("MoreScreen Content")
+                        }
+                    }
+                }
+
+                BottomNavigationBar(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .background(MR.colors.background_color.toCR()),
+                    selectedScreenType = mutableStateOf(selectedScreen),
+
+                    ) {
+                    selectedScreen = it
+                }
             }
+
         }
     }
 }
 
+
 @Composable
-fun BottomBar(menuSelected: (Int) -> Unit) {
-    BottomAppBar(
-        modifier = Modifier.fillMaxWidth().background(colorResource(MR.colors.background_color)),
-        backgroundColor = colorResource(MR.colors.background_color),
+fun BottomNavigationBar(
+    modifier: Modifier,
+    selectedScreenType: MutableState<ScreenType>,
+    onScreenSelected: (ScreenType) -> Unit
+) {
+    val items = listOf(
+        BottomNavItem("Home", Icons.Default.Home, ScreenType.HOME),
+        BottomNavItem("More", Icons.Default.Menu, ScreenType.MORE)
+    )
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        BottomBarItem(
-            text = "4\nBands",
-            isSelected = true,
-            onClick = { menuSelected.invoke(0) },
-            modifier = Modifier.weight(1f).padding(8.dp)
-        )
+        items.forEachIndexed { index, item ->
+            Spacer(modifier = Modifier.weight(1f))
+            BottomNavItemButton(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .alpha(if (item.screenType == selectedScreenType.value) 1f else 0.6f)
+                    .clickable {
+                        selectedScreenType.value = item.screenType
+                        onScreenSelected.invoke(item.screenType)
+                    }
+                    .padding(8.dp),
+                item = item,
+            )
+            if (index == items.lastIndex) {
+                Spacer(modifier = Modifier.weight(1f)) // Add Spacer with weight
+            }
+        }
 
-        BottomBarItem(
-            text = "5\nBands",
-            isSelected = false,
-            onClick = { menuSelected.invoke(1) },
-            modifier = Modifier.weight(1f).padding(8.dp)
-        )
-
-        BottomBarItem(
-            text = "6\nBands",
-            isSelected = false,
-            onClick = { menuSelected.invoke(2) },
-            modifier = Modifier.weight(1f).padding(8.dp)
-        )
-
-        BottomBarItem(
-            text = "CalculationGuide",
-            isSelected = false,
-            onClick = { menuSelected.invoke(3) },
-            modifier = Modifier.weight(1f).padding(8.dp)
-        )
     }
 }
 
 @Composable
-fun BottomBarItem(
-    text: String,
-    isSelected: Boolean,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val backgroundColor = if (isSelected) colorResource(MR.colors.black) else colorResource(MR.colors.gray)
-
-    Box(
+fun BottomNavItemButton(modifier: Modifier, item: BottomNavItem) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-            .clickable(onClick = onClick)
-            .background(color = backgroundColor)
     ) {
+        Icon(
+            imageVector = item.icon,
+            contentDescription = item.title,
+            tint = MR.colors.text_color.toCR()
+        )
+        Spacer(modifier = Modifier.height(4.dp))
         Text(
-            text = text,
-            color = if (isSelected) Color.White else Color.Gray,
-            textAlign = TextAlign.Center,
+            text = item.title,
             style = MaterialTheme.typography.caption,
-            modifier = Modifier.padding(8.dp)
+            color = MR.colors.text_color.toCR()
         )
     }
+}
+
+data class BottomNavItem(val title: String, val icon: ImageVector, val screenType: ScreenType)
+enum class ScreenType {
+    HOME,
+    MORE
 }
 
 @Composable
@@ -183,7 +220,7 @@ fun ColorSelectionDropdown(
                 ) {
                     Box(
                         modifier = Modifier
-                            .background(convertStringToComposeColor(color))
+                            .background(color.toComposeColor())
                             .size(24.dp)
                             .padding(4.dp)
                             .clip(CircleShape)
@@ -232,18 +269,3 @@ fun ResistanceColorIndicator(color: String?) {
     }
 }
 
-@Composable
-fun convertStringToComposeColor(hexColor: String): Color {
-    val mokoColor = dev.icerock.moko.graphics.Color.parseColor(hexColor)
-    return convertToComposeColor(mokoColor)
-}
-
-@Composable
-fun convertToComposeColor(mokoColor: MokoColor): Color {
-    return Color(
-        red = mokoColor.red / 255f,
-        green = mokoColor.green / 255f,
-        blue = mokoColor.blue / 255f,
-        alpha = mokoColor.alpha / 255f
-    )
-}
