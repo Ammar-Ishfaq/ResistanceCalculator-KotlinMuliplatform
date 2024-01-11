b2d4be3f373ea928bb4e045886e385252b18a340
ammar <ammarishfaq25@gmail.com>
2024-01-11 13:55:01 +0500

    Update ResistanceCalculator.kt

--- composeApp/src/commonMain/kotlin/com/ammar/resistorassistant/screens/ResistanceCalculator.kt
+++ composeApp/src/commonMain/kotlin/com/ammar/resistorassistant/screens/ResistanceCalculator.kt
@@ -1,35 +1,17 @@
 package com.ammar.resistorassistant.screens
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Absolute.Center
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -37,29 +19,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.semantics.SemanticsProperties.Heading
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import com.ammar.resistorassistant.MR
 import com.ammar.resistorassistant.extension.toCR
-import com.ammar.resistorassistant.extension.toComposeColor
-import com.ammar.resistorassistant.screens.band.FiveBandResistorCalculator
-import com.ammar.resistorassistant.screens.band.FourBandResistorCalculator
-import com.ammar.resistorassistant.screens.band.SixBandResistorCalculator
-import dev.icerock.moko.resources.compose.stringResource
+import com.ammar.resistorassistant.screens.band.*
 
 data object ResistanceCalculator : Screen {
     @Composable
@@ -181,55 +150,56 @@ enum class ScreenType {
 fun HomeScreenContent() {
     var selectedBand by remember { mutableStateOf(4) }
 
-    Column {
+    LazyColumn {
+        item {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .border(2.dp, Color.Black, shape = RoundedCornerShape(8.dp))
+            ) {
+                Text(
+                    text = "Select Band Type",
+                    color = Color.Black,
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(8.dp)
+                )
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .border(2.dp, Color.Black, shape = RoundedCornerShape(8.dp))
-        ) {
-            Text(
-                text = stringResource(MR.strings.select_band_type),
-                color = Color.Black,
-                style = MaterialTheme.typography.h5,
-                fontWeight = FontWeight.Thin,
-                modifier = Modifier.padding(8.dp)
-            )
-
-            BandSelectionMenu { selectedBands ->
-                selectedBand = selectedBands
-            }
-        }
-
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-                .border(2.dp, Color.Black, shape = RoundedCornerShape(8.dp))
-        ) {
-            Text(
-                text = stringResource(MR.strings.calculate_resistance),
-                color = Color.Black,
-                style = MaterialTheme.typography.h5,
-                fontWeight = FontWeight.Thin,
-                modifier = Modifier.padding(8.dp)
-            )
-            when (selectedBand) {
-                4 -> {
-                    FourBandResistorCalculator()
-                }
-                5->{
-                    FiveBandResistorCalculator()
-                }
-                6->{
-                    SixBandResistorCalculator()
+                BandSelectionMenu { selectedBands ->
+                    selectedBand = selectedBands
                 }
             }
         }
 
+        item {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .border(2.dp, Color.Black, shape = RoundedCornerShape(8.dp))
+            ) {
+                Text(
+                    text = "Calculate Resistance",
+                    color = Color.Black,
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(8.dp)
+                )
 
+                when (selectedBand) {
+                    4 -> {
+                        FourBandResistorCalculator()
+                    }
+                    5 -> {
+                        FiveBandResistorCalculator()
+                    }
+                    6 -> {
+                        SixBandResistorCalculator()
+                    }
+                }
+            }
+        }
     }
 }
 
